4a5a843884a3d7e06be4058bf5fa95629c97b4da
Vitalii.I <dekizber@gmail.com>
2024-07-04 22:27:01 +0300

    Final HW#7

--- js/task-6.js
+++ js/task-6.js
@@ -4,3 +4,32 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
+const createEl = document.querySelector('[data-create]').addEventListener('click', () => {
+  const inputEl = document.querySelector('#controls input');
+  const amount = parseInt(inputEl.value);
+
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    inputEl.value = '';
+  }
+});
+
+const destroyEl = document.querySelector('[data-destroy]').addEventListener('click', () => {
+  const boxEl = document.querySelector('#boxes').innerHTML = '';
+});
+
+function createBoxes(amount) {
+  const boxesContainer = document.querySelector('#boxes');
+  boxesContainer.innerHTML = '';
+
+  let size = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+    size += 10;
+  }
+}
